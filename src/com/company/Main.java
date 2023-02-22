1dcde587895940f1445556f6ffe12b0f64fcd86c
Anthony Jilwan <anthonyjilwan@gmail.com>
2023-02-23 00:36:35 +1100

    new stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+        String messageToPrint;
+        String time = "Night";
+        String mood = "Happy";
+
+        if(time == "Night" && mood == "Happy" ){
+            System.out.println("Match!!");
+        }
     }
 }
