671a4f7ff39b51dbf7d24b4a2f22a74a54eb6f2a
Queue-tipp <thomastacm@gmail.com>
2023-03-08 00:09:17 +1100

    updates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 public class Main {
     public static void main(String[] args) {
-        PracticeQuestions.reverseCharacterString("Hoe ass bitch");
+        PracticeQuestions.mulitplyByExponent();
     }
 }
